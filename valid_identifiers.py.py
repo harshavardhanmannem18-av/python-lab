0bf2f7007ee29b446c2c81ead58dcd2572db128b
harshavardhanmannem18-av <harshavardhanmannem18@gmail.com>
2026-07-28 20:29:13 +0530

    Delete valid_identifiers.py.py

--- valid_identifiers.py.py
+++
@@ -1,8 +0,0 @@
-	#Identify	which	of	the	following	are	valid	Python	identifiers	and	explain	why	the	invalid	ones	fail:
-#2value ,	invalid because identifiers cannot start with a digit
-#value_2 ,valid because it starts with a letter and contains only letters, digits, and underscores	
-#_hidden ,	valid because it starts with an underscore and contains only letters, digits, and underscores
-#class ,	invalid because it is a reserved keyword in Python
-#my-var ,	invalid because it contains a hyphen, which is not allowed in identifiers
-#MyClass ,	valid because it starts with a letter and contains only letters and digits
-#total$ ,	invalid because it contains a dollar sign, which is not allowed in identifiers
